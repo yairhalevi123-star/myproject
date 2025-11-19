defc21ba60ffd53229c365092ec2661121b47195
yairhalevi123-star <yairhalevi123@gmail.com>
2025-11-19 09:00:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+// var chack = document.querySelector(".input");
+// chack.addEventListener("click", () => {
+//   let body = document.querySelector("body");
+
+//   body.style.backgroundColor = "blue";
+// });
+var boga = document.querySelector(".Bugatti");
+boga.addEventListener("click", () => {
+  var price = document.querySelector(".price");
+  price.innerHTML = "100000$";
+  price.style.backgroundColor = "orange";
+  boga.innerHTML = "clicked";
+});
+boga.addEventListener("mouseout", () => {
+  var price = document.querySelector(".price");
+  price.innerHTML = "";
+  price.style.backgroundColor = "orage";
+  boga.innerHTML = "more...";
+});
+var englishh = document.querySelector(".english");
+englishh.addEventListener("click", () => {
+  var text = document.querySelector(".textt");
+  text.innerHTML =
+    "Chiron Sport: Lighter and stiffer chassis, focusing on handling and agilit";
+});
+
+var hebrew = document.querySelector(".hebrew");
+hebrew.addEventListener("click", () => {
+  var textheb = document.querySelector(".textheb");
+  textheb.innerHTML = "fdcefewx";
+});
+
+var cahle = document.querySelector(".cahle");
+cahle.addEventListener("click", () => {
+  var price2 = document.querySelector(".price2");
+  price2.innerHTML = "20000$";
+  price2.style.backgroundColor = "orange";
+  cahle.innerHTML = "clicked";
+});
+cahle.addEventListener("mouseout", () => {
+  var price2 = document.querySelector(".price2");
+  price2.innerHTML = "";
+  price2.style.backgroundColor = "orage";
+  cahle.innerHTML = "more...";
+});
+
+var cahle2 = document.querySelector(".cahle2");
+cahle2.addEventListener("click", () => {
+  var price3 = document.querySelector(".price3");
+  price3.innerHTML = "20000$";
+  price3.style.backgroundColor = "orange";
+  cahle2.innerHTML = "clicked";
+});
+cahle2.addEventListener("mouseout", () => {
+  var price3 = document.querySelector(".price3");
+  price3.innerHTML = "";
+  price3.style.backgroundColor = "orage";
+  cahle2.innerHTML = "more...";
+});
+
+var lambu = document.querySelector(".lambu");
+lambu.addEventListener("click", () => {
+  var price4 = document.querySelector(".price4");
+  price4.innerHTML = "30000$";
+  price4.style.backgroundColor = "orange";
+  lambu.innerHTML = "clicked";
+});
+lambu.addEventListener("mouseout", () => {
+  var price4 = document.querySelector(".price4");
+  price4.innerHTML = "";
+  price4.style.backgroundColor = "orage";
+  lambu.innerHTML = "more...";
+});
+
+var paga = document.querySelector(".paga");
+paga.addEventListener("click", () => {
+  var price5 = document.querySelector(".price5");
+  price5.innerHTML = "125000$";
+  price5.style.backgroundColor = "orange";
+  paga.innerHTML = "clicked";
+});
+paga.addEventListener("mouseout", () => {
+  var price5 = document.querySelector(".price5");
+  price5.innerHTML = "";
+  price5.style.backgroundColor = "orage";
+  paga.innerHTML = "more...";
+});
+
+var tesl = document.querySelector(".tesl");
+tesl.addEventListener("click", () => {
+  var price6 = document.querySelector(".price6");
+  price6.innerHTML = "25000$";
+  price6.style.backgroundColor = "orange";
+  tesl.innerHTML = "clicked";
+});
+tesl.addEventListener("mouseout", () => {
+  var price6 = document.querySelector(".price6");
+  price6.innerHTML = "";
+  price6.style.backgroundColor = "orage";
+  tesl.innerHTML = "more...";
+});
